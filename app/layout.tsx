1ba485b6c3a8dad895476d315af9416975efe8c6
xnpelur <michael.s5102@gmail.com>
2024-08-08 16:09:45 +0300

    Sidebar moved to layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,14 @@
+import { Button } from "@/components/ui/button";
+import {
+    EnvelopeOpenIcon,
+    PaperPlaneIcon,
+    TrashIcon,
+    FileTextIcon,
+} from "@radix-ui/react-icons";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +23,47 @@ export default function RootLayout({
 }>) {
     return (
         <html>
-            <body className={inter.className}>{children}</body>
+            <body className={cn(inter.className, "flex")}>
+                <aside className="h-full w-64 space-y-4 bg-violet-800 p-4">
+                    <span className="text-xl font-semibold text-white">
+                        My Mail
+                    </span>
+                    <Button className="w-full bg-violet-500">
+                        Новое сообщение
+                    </Button>
+                    <div className="space-y-2">
+                        <Button
+                            variant="ghost"
+                            className="flex h-9 w-full justify-start px-2 py-0 text-white"
+                        >
+                            <EnvelopeOpenIcon className="mr-2 h-4 w-4" />
+                            <span>Входящие</span>
+                        </Button>
+                        <Button
+                            variant="ghost"
+                            className="flex h-9 w-full justify-start px-2 py-0 text-white"
+                        >
+                            <PaperPlaneIcon className="mr-2 h-4 w-4" />
+                            <span>Отправленные</span>
+                        </Button>
+                        <Button
+                            variant="ghost"
+                            className="flex h-9 w-full justify-start px-2 py-0 text-white"
+                        >
+                            <FileTextIcon className="mr-2 h-4 w-4" />
+                            <span>Черновики</span>
+                        </Button>
+                        <Button
+                            variant="ghost"
+                            className="flex h-9 w-full justify-start px-2 py-0 text-white"
+                        >
+                            <TrashIcon className="mr-2 h-4 w-4" />
+                            <span>Спам</span>
+                        </Button>
+                    </div>
+                </aside>
+                <main className="h-full w-10 flex-1">{children}</main>
+            </body>
         </html>
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,51 +1,7 @@
-import { Button } from "@/components/ui/button";
-import {
-    EnvelopeOpenIcon,
-    PaperPlaneIcon,
-    TrashIcon,
-    FileTextIcon,
-} from "@radix-ui/react-icons";
-
 export default function Home() {
     return (
-        <main className="flex h-full">
-            <div className="h-full w-64 space-y-4 bg-violet-800 p-4">
-                <h1 className="text-xl font-semibold text-white">My Mail</h1>
-                <Button className="w-full bg-violet-500">
-                    Новое сообщение
-                </Button>
-                <div className="space-y-2">
-                    <Button
-                        variant="ghost"
-                        className="flex h-9 w-full justify-start px-2 py-0 text-white"
-                    >
-                        <EnvelopeOpenIcon className="mr-2 h-4 w-4" />
-                        <span>Входящие</span>
-                    </Button>
-                    <Button
-                        variant="ghost"
-                        className="flex h-9 w-full justify-start px-2 py-0 text-white"
-                    >
-                        <PaperPlaneIcon className="mr-2 h-4 w-4" />
-                        <span>Отправленные</span>
-                    </Button>
-                    <Button
-                        variant="ghost"
-                        className="flex h-9 w-full justify-start px-2 py-0 text-white"
-                    >
-                        <FileTextIcon className="mr-2 h-4 w-4" />
-                        <span>Черновики</span>
-                    </Button>
-                    <Button
-                        variant="ghost"
-                        className="flex h-9 w-full justify-start px-2 py-0 text-white"
-                    >
-                        <TrashIcon className="mr-2 h-4 w-4" />
-                        <span>Спам</span>
-                    </Button>
-                </div>
-            </div>
-            <div className="h-full w-10 flex-1"></div>
-        </main>
+        <div className="p-4">
+            <h1 className="text-2xl font-semibold">Входящие</h1>
+        </div>
     );
 }
