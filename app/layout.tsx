1292e66c4e11d0a9cac3ed54278d67bd70c9b57c
xnpelur <michael.s5102@gmail.com>
2024-11-28 12:09:19 +0300

    Title and favicon changed

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ import { Button } from "@/components/ui/button";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "My Mail",
+    title: "Email Client",
 };
 
 export default function RootLayout({
